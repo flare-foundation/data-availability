353b0232b9acb9a9b5b11b00a3e03adfbc0600cf
Luka Avbreht <luka.avbreht@aflabs.si>
2024-10-16 16:04:08 +0200

    Initial views

--- fdc/views.py
+++ fdc/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from rest_framework import viewsets
+
+from fdc.models import AttestationResult
+
+
+class AttestationResultViewSet(viewsets.ReadOnlyModelViewSet):
+    lookup_field = "voting_round_id"
+
+    def get_queryset(self):
+        return AttestationResult.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
