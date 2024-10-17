49789b612af9ac52110c609df85547b91a44d6be
Matej Janežič <janezic.mj@gmail.com>
2024-10-17 18:48:15 +0200

    fix: use correct query syntax in fsp/status

--- fsp/serializers.py
+++ fsp/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 
 
-class SingleVotingRoundDetailSerializer(serializers.Serializer):
+class VotingRoundSerializer(serializers.Serializer):
     voting_round_id = serializers.IntegerField()
     start_timestamp = serializers.IntegerField()
 
 
 class VotingRoundStatusSerializer(serializers.Serializer):
-    active = SingleVotingRoundDetailSerializer()
-    latest_fdc = SingleVotingRoundDetailSerializer()
-    latest_ftso = SingleVotingRoundDetailSerializer()
+    active = VotingRoundSerializer()
+    latest_fdc = VotingRoundSerializer()
+    latest_ftso = VotingRoundSerializer()
--- fsp/views.py
+++ fsp/views.py
@@ -5,14 +5,14 @@ from configuration.config import config
 from fsp.models import ProtocolMessageRelayed
 
 from .epoch import VotingEpoch
-from .serializers import SingleVotingRoundDetailSerializer, VotingRoundStatusSerializer
+from .serializers import VotingRoundSerializer, VotingRoundStatusSerializer
 
 
 class FspViewSet(viewsets.GenericViewSet):
-    @extend_schema(responses=SingleVotingRoundDetailSerializer)
+    @extend_schema(responses=VotingRoundSerializer)
     @decorators.action(detail=False, methods=["get"], url_path="latest-voting-round")
     def latest_voting_round(self, request, *args, **kwargs):
-        self.serializer_class = SingleVotingRoundDetailSerializer
+        self.serializer_class = VotingRoundSerializer
 
         ve = VotingEpoch.now()
 
@@ -34,31 +34,29 @@ class FspViewSet(viewsets.GenericViewSet):
         data = {
             "active": {
                 "voting_round_id": ve.n,
-                "timestamp": ve.start_s,
+                "start_timestamp": ve.start_s,
             },
-            "latest_ftso": {"voting_round_id": -1, "timestamp": -1},
-            "latest_fdc": {"voting_round_id": -1, "timestamp": -1},
+            "latest_ftso": {"voting_round_id": -1, "start_timestamp": -1},
+            "latest_fdc": {"voting_round_id": -1, "start_timestamp": -1},
         }
 
         # NOTE: for this to work saving to DB must be atomic
         latest_ftso = (
-            ProtocolMessageRelayed.objects.get_queryset()
-            .filter("protocol_id" == config.ftso_provider.protocol_id)
+            ProtocolMessageRelayed.objects.filter(protocol_id=config.ftso_provider.protocol_id)
             .order_by("-voting_round_id")
             .first()
         )
         latest_fdc = (
-            ProtocolMessageRelayed.objects.get_queryset()
-            .filter("protocol_id" == config.fdc_provider.protocol_id)
+            ProtocolMessageRelayed.objects.filter(protocol_id=config.fdc_provider.protocol_id)
             .order_by("-voting_round_id")
             .first()
         )
         if latest_ftso is not None:
             vef = VotingEpoch(latest_ftso.voting_round_id)
-            data["latest_ftso"] = {"voting_round_id": vef.n, "timestamp": vef.start_s}
+            data["latest_ftso"] = {"voting_round_id": vef.n, "start_timestamp": vef.start_s}
         if latest_fdc is not None:
             vef = VotingEpoch(latest_fdc.voting_round_id)
-            data["latest_fdc"] = {"voting_round_id": vef.n, "timestamp": vef.start_s}
+            data["latest_fdc"] = {"voting_round_id": vef.n, "start_timestamp": vef.start_s}
 
         serializer = self.get_serializer(data)
 
