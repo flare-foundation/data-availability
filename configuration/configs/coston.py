82e7514b923b001bdff35f21488a77480be99782
andraz maier <andraz.maier@aflabs.com>
2024-11-04 13:50:43 +0100

    fix: add new names of env variables

--- configuration/configs/coston.py
+++ configuration/configs/coston.py
@@ -37,9 +37,9 @@ def get_config() -> Configuration:
     )
 
     starting_config = SyncingConfig(
-        start_height=int(e.get("START_HEIGHT", "24237186")),  # old one: 24862497
+        start_height=int(e.get("INDEXING_DEFAULT_HEIGHT", "24237186")),  # old one: 24862497
         max_processing_block_batch=50,
-        processing_sleep_cycle=int(e.get("SLEEP_CYCLE", "20")),
+        processing_sleep_cycle=int(e.get("INDEXING_SLEEP_CYCLE", "20")),
     )
 
     return Configuration(
--- configuration/configs/songbird.py
+++ configuration/configs/songbird.py
@@ -37,9 +37,9 @@ def get_config() -> Configuration:
     )
 
     starting_config = SyncingConfig(
-        start_height=int(e.get("START_HEIGHT", "73422149")),
+        start_height=int(e.get("INDEXING_DEFAULT_HEIGHT", "73422149")),
         max_processing_block_batch=50,
-        processing_sleep_cycle=int(e.get("SLEEP_CYCLE", "20")),
+        processing_sleep_cycle=int(e.get("INDEXING_SLEEP_CYCLE", "20")),
     )
 
     return Configuration(
