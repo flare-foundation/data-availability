105a2d65d67142a3c8a995fdb909b71963b85cd5
Matej Janežič <janezic.mj@gmail.com>
2024-11-05 11:12:05 +0100

    chore: rewrite serializer to be more idiomatic

--- fdc/serializers/data.py
+++ fdc/serializers/data.py
@@ -2,21 +2,10 @@ from rest_framework import serializers
 
 from ..models import AttestationResult
 
-# class AttestationResultSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = AttestationResult
-#         # TODO:(matej) do we need all
-#         fields = "__all__"
-
 
 class AttestationMinimalProofSerializer(serializers.ModelSerializer):
-    response = serializers.ReadOnlyField()
+    response = serializers.CharField(read_only=True)
 
     class Meta:
         model = AttestationResult
-
-        fields = (
-            "response",
-            "proof",
-        )
-        read_only_fields = ("response",)
+        fields = ("response", "proof")
