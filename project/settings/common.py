03c0f884c72bbb35ee67fd290c4e689b50e4cf15
Matej Janežič <janezic.mj@gmail.com>
2024-11-07 16:26:20 +0100

    feat: remove unneeded dep simple_history

--- project/settings/common.py
+++ project/settings/common.py
@@ -71,7 +71,6 @@ INSTALLED_APPS = [
     "django.contrib.postgres",
     # dependencies
     "rest_framework",
-    "simple_history",
     "corsheaders",
     "drf_spectacular",
     # our apps
@@ -93,7 +92,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "simple_history.middleware.HistoryRequestMiddleware",  # Injects user in simple history
 ]
 
 ROOT_URLCONF = "project.urls"
