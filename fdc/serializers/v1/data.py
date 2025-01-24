3e7bfa0b4959247e0948744e91f6dddc59956552
Matej Janežič <janezic.mj@gmail.com>
2025-01-24 17:03:36 +0100

    fix: add proof to raw serializer and 0x prefix bytes

--- fdc/serializers/v1/data.py
+++ fdc/serializers/v1/data.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from processing.utils import prefix_0x
+
 from ...models import AttestationResult
 
 
@@ -12,6 +14,11 @@ class AttestationResultV1Serializer(serializers.ModelSerializer):
 
 
 class AttestationResultRawV1Serializer(serializers.ModelSerializer):
+    response_hex = serializers.SerializerMethodField()
+
+    def get_response_hex(self, obj) -> str:
+        return prefix_0x(obj.response_hex)
+
     class Meta:
         model = AttestationResult
-        fields = ("response_hex", "attestation_type")
+        fields = ("response_hex", "attestation_type", "proof")
